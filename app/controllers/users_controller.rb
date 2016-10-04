792d8e4c856f74765a4fa99da986d6c89a5dbb43
david szyszkowski <try2murkme@gmail.com>
2016-10-04 13:31:50 -0700

    added to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,56 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
+
   end
 
   def new
   end
 
-  def show
+  def create
+    @user = User.new({
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
+      })
+
+      if @user.save
+        session[:user_id] = @user.id
+        redirect_to '/'
+      else
+        render :new
+      end
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      redirect_to '/'
+    else
+      render :edit
+    end
   end
 
-  def create
+  def make_admin
+    @user=User.find(params[:id])
+    @user.is_admin = true
+    @user.save
+    redirect_to '/'
+
   end
+
+private
+
+    def user_params
+
+      params.require(:user).permit(:email, :password, :password_confirmation, :is_admin)
+    end
+
+
 end
